68e131fe5f22ff3d6fb1b8d83e7f38ffa5a1e06a
Александр Грачев <A.grachev94@yandex.ru>
2021-08-01 17:19:05 +0300

    first commit

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static config.Credentials.credentials;
+import static java.lang.String.format;
+
+
+public class TestBase {
+    @BeforeAll
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.startMaximized = true;
+        Configuration.remote = format(
+                "https://%s:%s@%s/wd/hub/",
+                credentials.login(),
+                credentials.password(),
+                System.getProperty("selenoid")
+        );
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+}
